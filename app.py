f9785f6d014cd9890a781e76c5dc8864381e5ff0
jaeho0718@naver.com <jaeho0718@naver.com>
2018-08-11 12:36:28 +0900

    'a'

---
+++ app.py
@@ -0,0 +1,38 @@
+import urllib
+import json
+import os
+from flask import flask
+from flask import request
+from flask import.make_response
+
+app = Flask(__name__)
+
+@app.route('/webhook',method=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    print("Request:")
+    print(json.dumps(req,indent=4))
+    res = makeWebhookResult(req)
+    res = json.dumps(res,indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type']= 'application/json'
+    return res
+
+def makeWebhookResult(req):
+    if req.get("result").get("action") != 'interest':
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    zone = parameters.get("lunch")
+    speech = "급식은"+"낚지덮밥"
+    print("Respose:")
+    print(speech)
+    return {
+        "speech":speech,
+        "displayText":speech,
+        "source":"Bank"
+    }
+if __name__ == '__main__':
+    #port  = int(os.getenv('POST',5000))
+    app.run()
--- datasend.py
+++
@@ -1,7 +0,0 @@
-import json
-import urllib.request
-text = '테스트1234'
-d = {'displayText':text}
-params = json.dumps(d).encode("utf-8")
-req = urllib.request.Request(url, data=params,
-                             headers={'content-type': 'application/json'})
