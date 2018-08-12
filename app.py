67364a4b789f41a493426684cb6418ec01fa9f1e
jaeho0718@naver.com <jaeho0718@naver.com>
2018-08-12 10:30:11 +0900

    'a'

--- app.py
+++ app.py
@@ -61,6 +61,6 @@ def makeWebhookResult(req):
     }
 
 if __name__ == '__main__':
-    #port  = int(os.getenv('PORT',5000))
-    #app.run(debug=True,port=port,host = '0.0.0.0')
-    app.run()    
+    port  = int(os.getenv('PORT',5000))
+    app.run(debug=True,port=port,host = '0.0.0.0')
+    #app.run()    
